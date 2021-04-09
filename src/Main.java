f203112fbf85dcbb44dcfc30b55eb2781010e771
Alexander Ivankov <ffmoneyff@gmail.com>
2021-04-09 17:00:51 +0300

    changes

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ public class Main {
         CreditPaymentService service = new CreditPaymentService();
         double credit1year = service.calculate(0.0999, 1_000_000, 1);
         System.out.println(credit1year);
-
         double credit2year = service.calculate(0.0999, 1_000_000, 2);
         System.out.println(credit2year);
         double credit3year = service.calculate(0.0999, 1_000_000, 3);
